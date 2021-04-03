1d936066d8eb147c12eafba151244a1983d15ec2
Qadeer Hussain <qadeeerhussain494@gmail.com>
2021-04-03 10:48:42 +0500

    Just Practice

---
+++ app.js
@@ -0,0 +1,52 @@
+//alert("Hello World");
+var a=10;
+var b=6;
+var c=a*b;
+//alert(c);
+// window.alert(c)
+console.log(c);
+
+var a="world";
+var b="hello";
+// var p=b+a;
+ console.log(a+" "+b);
+
+
+  var a=20;
+var b="d"
+var c=a+b;
+console.log(c);
+
+var a=20;
+var b="2"
+var c=a*b;
+console.log(c);
+
+var a=20;
+var b="h"
+var c=a*b;
+console.log(c);
+
+var a=10;
+var b=5;
+var c=6;
+var d=a - b*c;
+console.log(d);
+
+var a=10;
+var b=a++;
+console.log(b);
+console.log(a);
+
+
+
+var a=10;
+var b=++a;
+console.log(b);
+
+
+var x=10;
+var y=x++ + ++x -x + x++ - ++x +x;
+//     10  + 12 - 12+12  -14 +14
+//     22
+console.log(y);
